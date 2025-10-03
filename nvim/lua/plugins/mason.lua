0a0474fb136b06b4506a714248a8c0e4ded6faa1
gpowell <Geoffery.Powell@samaritanministries.org>
2025-10-03 15:52:32 -0500

    Added nvim configs

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,41 @@
+local function configure()
+  local mason_ok, mason = pcall(require, "mason")
+  if not mason_ok then
+    return
+  end
+
+  local mason_lsp_ok, mason_lsp_config = pcall(require, "mason-lspconfig")
+  if not mason_lsp_ok then
+    return
+  end
+
+  mason.setup({
+    ui = {
+      border = "rounded",
+    },
+  })
+
+  mason_lsp_config.setup({
+    automatic_installation = true,
+  })
+
+  --vim.lsp.set_log_level('debug')
+
+  -- How diagnostics show up
+  vim.diagnostic.config({
+    virtual_text = false,
+    underline = true,
+    signs = true,
+  })
+
+  vim.lsp.enable("pylyzer")
+end
+
+return {
+  "mason-org/mason-lspconfig.nvim", -- easier to configure mason
+  dependencies = {
+    "mason-org/mason.nvim", -- tool to install lsp/dap/linter/formatters
+    "neovim/nvim-lspconfig", -- configure lsps
+  },
+  config = configure,
+}
---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,17 @@
+local function configure() 
+	require("oil").setup()
+	vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+end
+
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  ---opts = {},
+  config = configure, 
+  -- Optional dependencies
+  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
+}
